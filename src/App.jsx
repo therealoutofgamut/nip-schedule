5613b35dca934d07f8d275514e887710e24d495e
Marc Theilhaber <inotrope@gmail.com>
2026-02-16 15:42:26 +1100

    Identified wrong analyse package call - resolved.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useMemo, useEffect, useRef } from "react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from '@vercel/analytics/react';
 
 const SCHEDULE_DATA = [
   // Birth
